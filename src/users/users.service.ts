52e2473a8319b06edc18704619a116978ac6c532
antonio <antonio.virgone@telepass.com>
2024-07-04 16:17:13 +0200

    create api users

---
+++ src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+    private users = [];
+
+    findAll() {
+        return this.users;
+    }
+
+    create(user) {
+        this.users.push(user);
+    }
+}
