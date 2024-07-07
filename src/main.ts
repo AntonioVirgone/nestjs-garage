eaa22a37efe58a330cac3afbd88cdd6f624f7078
antonio <antonio.virgone@telepass.com>
2024-07-07 12:51:45 +0200

    add new query to find element

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/prisma-exception.filter';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useLogger(new Logger());
   await app.listen(3000);
 }
 bootstrap();
